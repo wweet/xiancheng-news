ce2e9f7a74d5c1df4e24840d8fa373925a14bfdf
wc <2115211963@qq.com>
2024-09-10 21:40:31 +0800

    业务逻辑实现

---
+++ src/main/java/com/wc/XianchengNewsApplication.java
@@ -0,0 +1,13 @@
+package com.wc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XianchengNewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XianchengNewsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wc/XianchengNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XianchengNewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
